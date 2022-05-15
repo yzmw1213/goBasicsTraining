caea5dc3d3039479186df7500c7a13d728647f62
AyumuShigeta <faliplvsg@gmail.com>
2022-05-15 18:24:54 +0900

    再起関数のスタックオーバーフロー回避について記述

--- recursion/main.go
+++ recursion/main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 // 再起関数
 func fact(n int) int {
-	if n == 0 { return 1}
+	// 無限呼び出しを回避するため、再起関数ではベースケースに対してreturnする処理を入れる。
+	// n == 0のケースをコメントアウトしプログラムを実行すると、スタックオーバーフローが発生する
+	if n == 0 {
+		return 1
+	}
 	return n * fact(n-1)
 }
 
